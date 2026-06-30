68662396525b69d0a729751d1af8668eaf7395a8
infbelial <makethemrepentasmodai@gmail.com>
2026-06-30 19:08:52 +0300

    Более цивильный подход к тексту

---
+++ Locale/enUS.lua
@@ -0,0 +1,7 @@
+-- Kinda backwards but ruRU is the default loclae, because it's the only supported locale
+if GetLocale() == "ruRU" then
+    return
+end
+
+EZTEXT_DUNGEON_FINDER   = "Dungeon finder"
+EZTEXT_RAID_FINDER      = "Raid finder"
---
+++ Locale/ruRU.lua
@@ -0,0 +1,2 @@
+EZTEXT_DUNGEON_FINDER   = "Поиск подземелий"
+EZTEXT_RAID_FINDER      = "Поиск рейда"
